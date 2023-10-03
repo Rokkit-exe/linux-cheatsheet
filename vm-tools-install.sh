136dbf36cd59bc040acf4548b685b587c3785038
Rokkit-exe <francki0723@gmail.com>
2023-10-03 17:38:00 -0400

    update vm-tools-install.sh

--- vm-tools-install.sh
+++ vm-tools-install.sh
@@ -2,15 +2,15 @@
 echo " For WMWare [1] \n For VirtualBox [2] \n For UTM [3]"
 read -p "Enter your choice: " choice
 
-if ["$choice" = 1]; then
+if ["$choice" -eq 1]; then
         # installing vmware tools
         sudo apt install open-vm-tools-desktop open-vm-tools -y
-elif ["$choice" = 2] then
+elif ["$choice" -eq 2]; then
         # installing Virtualbox tools
         sudo apt install virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms -y
-elif ["$choice" = 3] then
+elif ["$choice" -eq 3]; then
         # installing UTM tools
         sudo apt install sprice-vdagent spice-webdavd -y
-else
+else; then
     echo "Invalid choice"
 fi
