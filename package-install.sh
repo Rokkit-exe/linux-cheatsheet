4624a01fbeb876827dfb56497d6758f5b26f64f1
Rokkit-exe <francki0723@gmail.com>
2023-10-03 17:49:13 -0400

    update package-install.sh

--- package-install.sh
+++ package-install.sh
@@ -3,20 +3,16 @@ echo "basic bundle [1] (git python3 vim curl wget htop)\n
     all bundle [3] \n"
 read -p "Enter your choice: " choice
 
-if ["$choice" -e 1]
-    then
-    # install basic tools
-    sudo apt install -y git python3 vim curl wget htop
-elif ["$choice" -e 2]
-    then
-    # install hacking tools
-    sudo apt install -y nmap net-tools metasploit-framework aircrack-ng burpsuite wireshark ettercap-text-only john hydra nikto sqlmap
-elif ["$choice" -e 3]
-    then
-    # install all tools
-    sudo apt install -y git python3 vim curl wget htop nmap net-tools metasploit-framework aircrack-ng burpsuite wireshark ettercap-text-only john hydra nikto sqlmap
-else
-    echo "Invalid choice"
-fi
+case "$choice" in 
+    1) sudo apt install -y git python3 vim curl wget htop 
+    ;;
+    2) sudo apt install -y nmap net-tools metasploit-framework aircrack-ng burpsuite wireshark ettercap-text-only john hydra nikto sqlmap 
+    ;;
+    3) sudo apt install -y git python3 vim curl wget htop nmap net-tools metasploit-framework aircrack-ng burpsuite wireshark ettercap-text-only john hydra nikto sqlmap 
+    ;;
+    *) echo "Invalid choice" 
+    ;;
+esac 
+
 
 
