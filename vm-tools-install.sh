9e27ec07816de105bec078f30da21ac0badc650a
Rokkit-exe <francki0723@gmail.com>
2023-10-03 17:35:10 -0400

    update vm-tools-install.sh

--- vm-tools-install.sh
+++ vm-tools-install.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 echo " For WMWare [1] \n For VirtualBox [2] \n For UTM [3]"
 read -p "Enter your choice: " choice
 
-if ["$choice" -e 1]
+if ["$choice" = 1]
         # installing vmware tools
         sudo apt install open-vm-tools-desktop open-vm-tools -y
-elif ["$choice" -e 2]
+elif ["$choice" = 2]
         # installing Virtualbox tools
         sudo apt install virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms -y
-elif ["$choice" -e 3]
+elif ["$choice" = 3]
         # installing UTM tools
         sudo apt install sprice-vdagent spice-webdavd -y
 else
