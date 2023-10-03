65922986f2bc6a819d10c6cd542f03fd48e90aa4
Rokkit-exe <francki0723@gmail.com>
2023-10-03 18:00:48 -0400

    update package-install.sh

--- package-install.sh
+++ package-install.sh
@@ -6,7 +6,8 @@ read -p "Enter your choice: " choice
 case "$choice" in 
     1) sudo apt install -y git python3 vim curl wget htop 
     ;;
-    2) sudo apt install -y nmap net-tools metasploit-framework aircrack-ng burpsuite wireshark ettercap-text-only john hydra nikto sqlmap 
+    2) sudo apt install -y nmap net-tools aircrack-ng burpsuite wireshark ettercap-text-only john hydra nikto sqlmap
+    curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \chmod 755 msfinstall && \./msfinstall
     ;;
     3) sudo apt install -y git python3 vim curl wget htop nmap net-tools metasploit-framework aircrack-ng burpsuite wireshark ettercap-text-only john hydra nikto sqlmap 
     ;;
