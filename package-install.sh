15decd6b2b92ce085271e7f3970c262a403aba2a
Rokkit-exe <francki0723@gmail.com>
2023-10-03 17:45:20 -0400

    update vm-tools-install.sh

--- package-install.sh
+++ package-install.sh
@@ -4,12 +4,15 @@ echo "basic bundle [1] (git python3 vim curl wget htop)\n
 read -p "Enter your choice: " choice
 
 if ["$choice" -e 1]
+    then
     # install basic tools
     sudo apt install -y git python3 vim curl wget htop
 elif ["$choice" -e 2]
+    then
     # install hacking tools
     sudo apt install -y nmap net-tools metasploit-framework aircrack-ng burpsuite wireshark ettercap-text-only john hydra nikto sqlmap
 elif ["$choice" -e 3]
+    then
     # install all tools
     sudo apt install -y git python3 vim curl wget htop nmap net-tools metasploit-framework aircrack-ng burpsuite wireshark ettercap-text-only john hydra nikto sqlmap
 else
