ff30ad876bc3c9a04d2809423ae1600fdf07d3bf
Rokkit-exe <francki0723@gmail.com>
2023-10-09 14:57:13 -0400

    adding bashrc-set-up.sh

---
+++ config-script/bashrc-set-up.sh
@@ -0,0 +1,15 @@
+cd 
+
+echo installing git
+sudo apt install git -y
+
+echo cloning bash-it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+
+echo installing bash-it
+~/.bash_it/install.sh
+
+echo installing atomic theme
+sed -i 's/'"$BASH_IT_THEME"'/atomic/g' ~/.bashrc
+
+
