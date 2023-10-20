38e250c0a527e1b21157d2c54d1752c6a2d004bf
Rokkit-exe <francki0723@gmail.com>
2023-10-20 14:47:51 -0400

    adding vs-codium-install.sh

---
+++ package-installers/vs-codium-install.sh
@@ -0,0 +1,14 @@
+# Add the GPG key of the repository:
+
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+#Add the repository:
+
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+Update then install vscodium (if you want vscodium-insiders, then replace codium by codium-insiders):
+
+# install with apt
+sudo apt update && sudo apt install codium
